39ed99b675c742145f7ae02adb5973be3d9d1908
AnitaAnn <nohyehanita@gmail.com>
2023-02-07 19:19:54 -0800

    low level lang

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,34 +1,19 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @n: data to insert in the new element
+ * free_listint - frees a linked list
+ * @head: head of the list
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: void
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+void free_listint(listint_t *head)
 {
-    listint_t *new;
-    listint_t *temp = *head;
+	listint_t *temp;
 
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-
-    while (temp->next)
-        temp = temp->next;
-
-    temp->next = new;
-
-    return (new);
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
